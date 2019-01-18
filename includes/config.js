540ab5f9db5d06bb757a467f16887b2a2bfd0de1
Johannes Scribante <johannes.scri@gmail.com>
2019-01-18 11:30:58 +0200

    update - basic server setup

---
+++ includes/config.js
@@ -0,0 +1,20 @@
+const config = require('./config.json');
+let env;
+let params;
+
+const setup = () => {
+    if (process.env.ENV) {
+        env = process.env.ENV;
+    } else {
+        env = "development";
+        params = config[env];
+
+        Object.keys(params).forEach(key => {
+            process.env[key.toUpperCase()] = params[key];
+        });
+    }
+};
+
+module.exports = {
+    setup
+};
--- server.js
+++ server.js
@@ -1,8 +1,23 @@
-let Express = require('express');
+const Express = require('express');
+const CORS = require('cors');
+const bodyParser = require('body-parser');
+// const logger = require('logger');
 
-let PORT = 3000;
+const {setup} = require('./includes/config.js');
+
+setup();
+
+let PORT = process.env.NODE_SERVER_PORT;
 let app = Express();
 
+// app.use(logger);
+app.use(CORS({
+    origin: true,
+    methods: ['GET', 'PUT', 'POST', 'PATCH', 'OPTIONS'],
+    optionsSuccessStatus: 200,
+    credentials: true
+}));
+app.use(bodyParser.json());
 
 app.listen(() => {
     console.log(`Express server listening on port ${PORT}`);
